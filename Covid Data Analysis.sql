68500bad128811d5101b6acf475a06fa38fd82bf
Nach0R3P3TT0 <123325793+Nach0R3P3TT0@users.noreply.github.com>
2023-04-04 17:11:33 -0300

    Update Covid Data Analysis.sql

--- Covid Data Analysis.sql
+++ Covid Data Analysis.sql
@@ -1,31 +1,78 @@
+/* 
+COVID 19 Data Analysis 
+Main skills used: Aggregate functions, Joins, CTE's, Window Functions, View's, Converting data types
+*/
+
 select *
 from CovidData..CovidDeaths
 where continent is not null
 order by 3,4;
-
---select *
---from CovidData..CovidVaccinations
---order by 3,4;
 
 -- select relevant data
 select location, date, total_cases, new_cases, total_deaths, population
 from CovidData..CovidDeaths
 order by 1,2;
 
---Rate of total-cases vs total_deaths
-select location, date, cast(total_cases as numeric) as total_cases, cast(total_deaths as numeric) as total_deaths, 
-(cast(total_deaths as  numeric)/(cast(total_cases as numeric)))*100 as DeathPercentage
+select sum(cast(new_cases as numeric)) as total_cases, sum(cast(new_deaths as numeric)) as total_deaths, 
+sum(cast(new_deaths as  numeric))/sum(cast(new_cases as numeric))*100 as LethalityRate
 from CovidData..CovidDeaths
 where continent is not null
 --and location like '%Argentina%'
 order by 1,2;
 
+select location, date, cast(total_cases as numeric) as total_cases, cast(total_deaths as numeric) as total_deaths, 
+(cast(total_deaths as  numeric)/(cast(total_cases as numeric)))*100 as LethalityRate
+from CovidData..CovidDeaths
+where continent is not null
+--and location like '%Argentina%'
+order by 1,2;
+
+--Rate of total deaths vs total cases, this ratio is commonly known as lethality. 
+select location, date, cast(total_cases as numeric) as total_cases, cast(total_deaths as numeric) as total_deaths, 
+(cast(total_deaths as  numeric)/(cast(total_cases as numeric)))*100 as LethalityRate
+from CovidData..CovidDeaths
+where continent is not null
+--and location like '%Argentina%'
+order by 1,2;
+
+--we can use it to calculate the general lethality of the virus for each specific location or continent.
+With GeneralLethality (location, daily_cases, daily_deaths, DailyLethalityRate)
+as (
+select location, cast(new_cases as numeric) as daily_cases, cast(new_deaths as numeric) as daily_deaths, 
+cast(new_deaths as numeric)/cast(new_cases as numeric)*100 as DailyLethalityRate
+from CovidData..CovidDeaths
+where continent is not null
+)
+select *, sum(DailyLethalityRate)/COUNT(date)
+from GeneralLethality
+;
+/*We can calculate the moving-average case fatality rate (CFR fatality=lethality) ratio between 7 days average number of deaths and the 7
+days average number of cases 10 days earlier. We usually use multiples of 7 for this average, because a week has 7 days so we include each day
+the same number of times.*/
+with CFR (location, date, total_cases, total_deaths, Deaths_7DaysAVG, Cases10_7DaysAVG) as 
+(
+SELECT
+  location,
+  date,
+  CAST(total_cases AS float) AS total_cases,
+  CAST(total_deaths AS float) AS total_deaths,
+  AVG(CAST(total_deaths AS float)) OVER (PARTITION BY location ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS deaths_7day_avg,
+  AVG(CAST(total_cases AS float)) OVER (PARTITION BY location ORDER BY date ROWS BETWEEN 16 PRECEDING AND 10 PRECEDING) AS cases_10day_avg
+from CovidData..CovidDeaths
+where continent is not null
+)
+select *, Deaths_7DaysAVG/Cases10_7DaysAVG as Moving_CFRatio
+from CFR;
+
 --Looking at total cases vs population
-select location, date, total_cases, population, (cast(total_cases as numeric)/population)*100 as PercentPopulationInfected
+select location, date, population, MAX(cast(total_cases as numeric)) HighestInfectionCount, 
+ROUND(max(cast(total_cases as INT)/CAST(population AS float))*100,2,2) as PercentPopulationInfected
 from CovidData..CovidDeaths
 --where location like '%Argentina%'
 where continent is not null
-order by 1,2;
+group by location, population,date
+order by PercentPopulationInfected DESC
+;
 
 --countries with highest infection rate compared to population
 select location, population, max(total_cases) as HighestInfectionCount, max(cast(total_cases as numeric)/population)*100 as PercentPopulationInfected
@@ -68,6 +115,7 @@ order by HighestDeathCount DESC;
 select location, max(cast(total_deaths as int)) as HighestDeathCount
 from CovidData..CovidDeaths
 where continent is null and location not like '%income%'
+and location not in('World', 'European Union','International')
 group by location
 order by HighestDeathCount DESC;
 
@@ -76,6 +124,7 @@ create view ContinentDeathCount as
 select location, max(cast(total_deaths as int)) as HighestDeathCount
 from CovidData..CovidDeaths
 where continent is null and location not like '%income%'
+and location not in('World', 'European Union','International')
 group by location
 ;
 select * from ContinentDeathCount;
@@ -99,6 +148,25 @@ from CovidData..CovidDeaths
 where continent is not null
 ;
 select * from LethalityMortalityRate;
+
+--Exploring countries on their total cases, total deaths, population, Percent of the population infected, percent of the population deceased
+select location, population, sum(new_cases) as TotalCases, sum(cast(new_deaths as numeric)) as TotalDeaths, 
+sum(new_cases)/population*100 as PercentPopulationInfected, sum(cast(new_deaths as numeric))/population*100 as PercentPopulationDeceased
+from CovidData..CovidDeaths
+where continent is not null
+group by location, population
+order by 3 DESC;
+
+--Alternative way of exploring total cases, total deaths, population, Percent of the population infected, percent of the population deceased
+--using the function max
+select location, population, max(cast(total_cases as numeric)) as Max_Cases, max(cast(total_deaths as numeric)) as Max_Deaths, 
+max(cast(total_cases as numeric))/population*100 as PercentPopulationInfected, max(cast(total_deaths as numeric))/population*100 as PercentPopulationDeceased
+from CovidData..CovidDeaths
+where continent is not null
+group by location, population
+order by 3 DESC;
+
+
 
 -- looking at total population vs vaccinations, what is the total mamount of vaccinated people in the world. Notice that when we use 'over
 --partition by...' we shouldn't use group by.
@@ -199,3 +267,4 @@ where dea.continent is not null
 
 select *
 from CumulativePopulationVaccinated;
+
